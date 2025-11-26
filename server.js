96efd87f0af5b76d5b18cbcc719b98e801eb5832
Aman Singh <amankumarkvm4@gmail.com>
2025-11-26 14:48:40 +0530

    preparing the API server

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-
 require('dotenv').config()
-const api = process.env.API_URL
+const cors = require('cors')
+const authJwt = require('./helpers/jwt')
+const errorHandler = require('./helpers/error-handler')
 
+app.use(cors())
+app.options('*', cors())
 
-//
-const productsRouter = require('./routers/products')
 
 
 //middleware
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
+app.use(authJwt())
+app.use(errorHandler)
+
 
 //Routers
+const productsRouter = require('./routers/products')
+const categoriesRouter = require('./routers/categories')
+const usersRouter = require('./routers/users')
+const ordersRouter = require('./routers/orders')
+
+const api = process.env.API_URL
+
 app.use(`${api}/products`, productsRouter)
+app.use(`${api}/categories`, categoriesRouter)
+app.use(`${api}/users`, usersRouter)
+app.use(`${api}/orders`, ordersRouter)
+
 
 
 mongoose.connect(process.env.CONNECTION_STRING)
