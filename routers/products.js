a1bd8173f030eaa469aec65d4af150c23aab61c2
Aman Singh <amankumarkvm4@gmail.com>
2025-11-27 22:24:22 +0530

    setup products and categories APIs

--- routers/products.js
+++ routers/products.js
@@ -2,27 +2,30 @@ const Category = require('../models/category')
 const Product = require('../models/product')
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find()
-    res.send(productList)
+  const productList = await Product.find().populate('category')
+  res.send(productList)
 })
 
 router.post(`/count`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock
+  })
 
-    product.save().then((createdProduct) => {
-        res.status(201).send(createdProduct)
-    }).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
+  product.save().then((createdProduct) => {
+    res.status(201).send(createdProduct)
+  }).catch((err) => {
+    res.status(500).json({
+      error: err,
+      success: false
     })
+  })
 })
 
 router.post('/', async (req, res) => {
@@ -42,7 +45,7 @@ router.post('/', async (req, res) => {
       description: req.body.description,
       richDescription: req.body.richDescription,
       image: req.body.image,
-      images: req.body.images,         
+      images: req.body.images,
       brand: req.body.brand,
       price: req.body.price,
       category: req.body.category,
@@ -76,5 +79,78 @@ router.post('/', async (req, res) => {
     });
   }
 });
+
+router.get(`/:id`, async (req, res) => {
+  const product = await Product.findById(req.params.id).populate('category')
+
+  if (!product) {
+    res.status(500).json({ success: false })
+  }
+  res.send(product)
+})
+
+router.put('/:id', async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).send('Invalid Product Id');
+  }
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    return res.status(400).send('Invalid Category');
+  }
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      images: req.body.images,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
+  if (!product)
+    return res.status(500).send('The product cannot be updated!');
+  res.send(product);
+});
+
+// Delete category
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message
+    });
+  }
+});
+
+router.get('/get/count', async (req, res) => {
+  try {
+    const productCount = await Product.countDocuments();  // Counts all products
+
+    res.status(200).json({ success: true, productCount });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
 
 module.exports = router
