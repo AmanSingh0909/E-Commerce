dd276a0f85de068c6f7fcd281b04614c6596b55d
Aman Singh <amankumarkvm4@gmail.com>
2025-12-11 20:36:33 +0530

    updating order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,21 +4,28 @@ const { getordersController, getOrdersbyIdController, createOrderController, upd
 
 const router = express.Router()
 
-
+// get orders 
 router.get(`/`, getordersController )
 
+// get orders by id 
 router.get(`/:id`, getOrdersbyIdController)
 
+//create orders
 router.post('/', createOrderController)
 
+// update orders
 router.put(`/:id`, updateOrdersByIdController)
 
+// delete orders by id
 router.delete("/:id", deleteOrdersByIdController)
 
+// get totalsales
 router.get('/get/totalsales', getOrderTotalSalesController)
 
+// get orders counts
 router.get('/get/count', getOrdesCountsController)
 
+// get user orders by user id
 router.get('/get/userorders/:userid', getUserOrdersByUserIdController)
 
 module.exports = router
